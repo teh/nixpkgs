221478afcfaf48e634b9aa944c12b72d12c3fdc3
Tom Hunger <tehunger@gmail.com>
2016-05-30 15:55:10 +0100

    init: dex at 0.4.0

--- pkgs/top-level/go-packages.nix
+++ pkgs/top-level/go-packages.nix
@@ -854,6 +854,20 @@ let
     buildInputs = [ pkgs.libpcap tablewriter ];
   };
 
+  dex = buildFromGitHub {
+    rev    = "v0.4.0";
+    owner  = "coreos";
+    repo   = "dex";
+    sha256 = "1cyf9zsn9v57rrr6q908adhmqa0la0xd62bhmia66s5mi2f9s76s";
+
+    # dex comes with all its dependencies vendored
+    preBuild = ''
+      export GO15VENDOREXPERIMENT=1
+    '';
+
+    buildInputs = [ ldap asn1-ber ];
+  };
+
   fsnotify.v0 = buildGoPackage rec {
     rev = "v0.9.3";
     name = "fsnotify.v0-${rev}";
