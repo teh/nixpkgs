1a91159493063fc3badbda7c5ec032b27d58c6ec
Jan Tojnar <jtojnar@gmail.com>
2017-08-08 01:35:54 +0200

    gnome: further changes 3.20 -> 3.24

--- pkgs/desktops/gnome-3/3.24/misc/gspell/src.nix
+++ pkgs/desktops/gnome-3/3.24/misc/gspell/src.nix
@@ -1,10 +1,10 @@
 fetchurl: rec {
-  major = "1.2";
+  major = "1.4";
   minor = "1";
   name = "gspell-${major}.${minor}";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gspell/${major}/${name}.tar.xz";
-    sha256 = "18zisdrq14my2iq6iv3lhqfn9jg98bqwbzcdidp7hfk915gkw74z";
+    sha256 = "1ghh1xdzf04mfgb13zqpj88krpa44xv2vbyhm6k017kzrpz8hbs4";
   };
 }
