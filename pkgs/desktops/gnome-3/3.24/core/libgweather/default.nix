e77ee04c2ddbd893f618e88aac4c17015379b12e
Tom Hunger <tehunger@gmail.com>
2017-08-27 12:23:50 +0100

    Remove manaul override for GIR files for gnome weather.
    This makes it start again.

--- pkgs/desktops/gnome-3/3.24/core/libgweather/default.nix
+++ pkgs/desktops/gnome-3/3.24/core/libgweather/default.nix
@@ -4,8 +4,6 @@
 stdenv.mkDerivation rec {
   inherit (import ./src.nix fetchurl) name src;
 
-  makeFlags = "INTROSPECTION_GIRDIR=$(out)/share/gir-1.0/ INTROSPECTION_TYPELIBDIR=$(out)/lib/girepository-1.0";
-
   configureFlags = [ "--with-zoneinfo-dir=${tzdata}/share/zoneinfo" ];
   propagatedBuildInputs = [ libxml2 gtk libsoup gconf pango gdk_pixbuf atk gnome3.geocode_glib ];
   nativeBuildInputs = [ pkgconfig intltool ];
