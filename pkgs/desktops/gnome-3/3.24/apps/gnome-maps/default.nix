c17688fd69f1ea1950799d5f3d30d07ebb0111a9
Jan Tojnar <jtojnar@gmail.com>
2017-08-28 15:02:25 +0200

    gnome-maps: fix dbus activation

--- pkgs/desktops/gnome-3/3.24/apps/gnome-maps/default.nix
+++ pkgs/desktops/gnome-3/3.24/apps/gnome-maps/default.nix
@@ -1,5 +1,5 @@
 { stdenv, fetchurl, intltool, pkgconfig, gnome3, gtk3
-, gobjectIntrospection, gdk_pixbuf, librsvg, autoreconfHook
+, gobjectIntrospection, gdk_pixbuf, librsvg, libgweather, autoreconfHook
 , geoclue2, wrapGAppsHook, folks, libchamplain, gfbgraph, file, libsoup
 , webkitgtk }:
 
@@ -11,10 +11,20 @@ stdenv.mkDerivation rec {
   buildInputs = [ pkgconfig intltool gobjectIntrospection wrapGAppsHook
                   gtk3 geoclue2 gnome3.gjs gnome3.libgee folks gfbgraph
                   gnome3.geocode_glib libchamplain file libsoup
-                  gdk_pixbuf librsvg autoreconfHook
+                  gdk_pixbuf librsvg libgweather autoreconfHook
                   gnome3.gsettings_desktop_schemas gnome3.evolution_data_server
                   gnome3.gnome_online_accounts gnome3.defaultIconTheme
                   webkitgtk ];
+
+  # The .service file isn't wrapped with the correct environment
+  # so misses GIR files when started. By re-pointing from the gjs
+  # entry point to the wrapped binary we get back to a wrapped
+  # binary.
+  preConfigure = ''
+    substituteInPlace "data/org.gnome.Maps.service.in" \
+        --replace "Exec=@pkgdatadir@/org.gnome.Maps" \
+                  "Exec=$out/bin/gnome-maps"
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://wiki.gnome.org/Apps/Maps;
